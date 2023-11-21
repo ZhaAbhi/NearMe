5098f344ceccbb668a41d62969f10a1834c80f31
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-11-21 17:26:20 +0545

    Added two screen

---
+++ screens/SellServiceScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const SellServiceScreen = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <Text>Be the seller</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SellServiceScreen;
---
+++ screens/ServiceStatusScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const ServiceStatusScreen = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <Text>Be the seller</Text>
+    </SafeAreaView>
+  );
+};
+
+export default ServiceStatusScreen;
