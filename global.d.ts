374fa82bdf5458ba7525eccbb3e9cf2bbf967d31
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-11-22 10:37:04 +0545

    Finalizing bottom tab navigation

--- global.d.ts
+++ global.d.ts
@@ -15,3 +15,17 @@ declare module 'react-native-vector-icons/Entypo' {
   const Entypo: Icon;
   export default Entypo;
 }
+
+declare module 'react-native-vector-icons/Foundation' {
+  import {Icon} from 'react-native-vector-icons/Icon';
+
+  const Foundation: Icon;
+  export default Foundation;
+}
+
+declare module 'react-native-vector-icons/MaterialCommunityIcons' {
+  import {Icon} from 'react-native-vector-icons/Icon';
+
+  const MaterialCommunityIcons: Icon;
+  export default MaterialCommunityIcons;
+}
--- navigation/MainNavigation.tsx
+++ navigation/MainNavigation.tsx
@@ -3,21 +3,42 @@ import HomeScreen from '../screens/HomeScreen';
 import SellServiceScreen from '../screens/SellServiceScreen';
 import ServiceStatusScreen from '../screens/ServiceStatusScreen';
 import HomeIcon from 'react-native-vector-icons/Entypo';
+import SellerIcon from 'react-native-vector-icons/Foundation';
+import StatusIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const MyTab = createBottomTabNavigator();
 
 const MainNavigation = (): JSX.Element => {
   return (
-    <MyTab.Navigator screenOptions={{headerShown: false}}>
+    <MyTab.Navigator
+      screenOptions={{headerShown: false, tabBarActiveTintColor: '#b88804'}}>
       <MyTab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          tabBarIcon: () => <HomeIcon name="home" size={25} />,
+          tabBarIcon: ({size, color}) => (
+            <HomeIcon name="home" size={size} color={color} />
+          ),
         }}
       />
-      <MyTab.Screen name="Seller" component={SellServiceScreen} />
-      <MyTab.Screen name="Status" component={ServiceStatusScreen} />
+      <MyTab.Screen
+        name="Seller"
+        component={SellServiceScreen}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <SellerIcon name="torso-business" size={size} color={color} />
+          ),
+        }}
+      />
+      <MyTab.Screen
+        name="Status"
+        component={ServiceStatusScreen}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <StatusIcon name="update" size={size} color={color} />
+          ),
+        }}
+      />
     </MyTab.Navigator>
   );
 };
