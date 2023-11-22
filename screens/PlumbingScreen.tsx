54d6e374d669efc4537807f28edeb33378fd776c
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-11-22 13:46:39 +0545

    Designed header of plumbing category

--- screens/PlumbingScreen.tsx
+++ screens/PlumbingScreen.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
-import {SafeAreaView, Text, TouchableOpacity} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageSourcePropType,
+  StyleSheet,
+  TextInput,
+} from 'react-native';
+
+import LoacationPin from 'react-native-vector-icons/Entypo';
+import BackIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+interface PlumberType {
+  id: number;
+  name: string;
+  avatar: ImageSourcePropType;
+  rating: number;
+}
 
 interface PlumbingScreenProps {
   route: any;
   navigation: any;
 }
+
+const Data: PlumberType[] = [
+  {
+    id: 1,
+    name: 'Kamlesh Gupta',
+    avatar: require('../assets/NearMe-Plumber-Assets/Kamlesh.jpeg'),
+    rating: 4,
+  },
+  {
+    id: 2,
+    name: 'Sumit Shah',
+    avatar: require('../assets/NearMe-Plumber-Assets/sumit.jpeg'),
+    rating: 5,
+  },
+  {
+    id: 3,
+    name: 'Aman Mandal',
+    avatar: require('../assets/NearMe-Plumber-Assets/Aman.jpeg'),
+    rating: 5,
+  },
+];
 
 const PlumbingScreen: React.FC<PlumbingScreenProps> = ({
   navigation,
@@ -13,13 +52,111 @@ const PlumbingScreen: React.FC<PlumbingScreenProps> = ({
   const {description} = route.params;
   return (
     <SafeAreaView>
-      <Text>This is the plumbing screen</Text>
-      <Text>{description}</Text>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text>Go Back</Text>
-      </TouchableOpacity>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={styles.backIcon}>
+            <BackIcon name="keyboard-backspace" size={25} color="white" />
+          </TouchableOpacity>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>Search For </Text>
+            <Text style={styles.title}>Plumbers</Text>
+          </View>
+        </View>
+        <View style={styles.inputContainer}>
+          <View>
+            <Text style={styles.currentLocationTitle}>
+              Your Current Location
+            </Text>
+            <View style={styles.currentLocationContainer}>
+              <LoacationPin
+                name="location-pin"
+                size={25}
+                style={styles.locationpin}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Kathmandu, Nepal"
+                placeholderTextColor="black"
+              />
+            </View>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 5,
+    height: '45%',
+    borderBottomWidth: 3.5,
+    borderStartWidth: 2,
+    borderEndWidth: 2,
+    borderColor: 'lightgrey',
+    borderBottomStartRadius: 25,
+    borderBottomEndRadius: 25,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  backIcon: {
+    height: 40,
+    width: 40,
+    backgroundColor: '#b88804',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: '16%',
+  },
+  title: {
+    fontSize: 18,
+    color: 'black',
+  },
+  inputContainer: {
+    marginTop: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  currentLocationTitle: {
+    fontSize: 16,
+    marginBottom: 8,
+    fontWeight: '300',
+    color: 'black',
+    marginLeft: 10,
+  },
+  currentLocationContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  locationpin: {
+    position: 'absolute',
+    marginLeft: 5,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 25,
+    paddingLeft: 30,
+  },
+});
 
 export default PlumbingScreen;
